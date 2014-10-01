1f93e0f2a49e7d7b7a4fa7303895088f498b79e7
Ryan Duan <duanpanda@gmail.com>
2014-10-01 19:57:24 -0400

    improve auto test for 2(b)

--- ai/ex1/ex1.pl
+++ ai/ex1/ex1.pl
@@ -78,9 +78,9 @@ tree_ins(X, t(L, K, R), t(L, K, NewR)) :-
 %% Query:
 %%   test([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14], X).
 %% Pass
-test([],[]).
-test([H|T],[X|R]) :-
+test([]).
+test([H|T]) :-
 	Tree = t(t(t(nil,1,nil), 3, t(nil,5,nil)), 7, t(t(nil,9,nil), 11, t(nil,13,nil))),
 	tree_ins(H, Tree, X),
 	write(X),nl,
-	test(T, R).
+	test(T).
