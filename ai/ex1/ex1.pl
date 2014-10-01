3bbe3f51997ee219255153574fbc45850e1fc5dd
Ryan Duan <duanpanda@gmail.com>
2014-10-01 17:22:54 -0400

    correct solution to 2(a), plus Sabah's version

--- ai/ex1/ex1.pl
+++ ai/ex1/ex1.pl
@@ -21,17 +21,34 @@ sub(X, Y, [], []).
 %%% 2(a)
 % test data:
 %   t(t(t(nil,1,nil), 2, t(nil,3,nil)), 4, t(t(nil,5,nil), 6, t(nil,7,nil)))
+% almost the same as Dr. Mohammed's version
 less(X, t(L, K, R)) :-
-	less(X, K),
+	X < K,			% replaced 'less(X, K)' with 'X < K', because
+				% X and K are atoms, they cannot be t(L,K,R).
 	less(X, L),
 	less(X, R), !.
 less(t(L, K, R), X) :-
-	less(K, X),
+	K < X,			% replaced 'less(K, X)' with 'K < X'
 	less(L, X),
 	less(R, X), !.
 less(nil, X) :- !.
 less(X, nil) :- !.
-less(X, Y) :- X < Y, !.
+% less(X, Y) :- X < Y.         % deleted this to avoid cases like '3 < nil'
 orderedT(t(L, K, R)) :-
 	less(L, K),
 	less(K, R).
+
+%% Dr. Sabah Mohammed's version
+orderedT( t(L, K, R) ) :-
+	ordered_left(L, K),
+	ordered_right(K, R).
+ordered_left(nil, N).		% we can add ':- !.' here
+ordered_left(t(L, K, R), N) :-
+	K < N,
+	ordered_left(L, K),
+	ordered_right(K, R).
+ordered_right(N, nil).		% we can add ':- !.' here
+ordered_right(N, t(L, K, R)) :-
+	N < K,
+	ordered_left(L, K),
+	ordered_right(K, R).
