2913245f369534295c7cbe5306dd24a16f8a9a03
Ryan Duan <duanpanda@gmail.com>
2014-10-01 17:09:17 -0400

    First solution to 2(a)

--- ai/ex1/ex1.pl
+++ ai/ex1/ex1.pl
@@ -17,3 +17,21 @@ permutation([], []).
 sub(X, Y, [H|T], [H|R]) :- H \= X, sub(X, Y, T, R).
 sub(X, Y, [X|T], [Y|R]) :- sub(X, Y, T, R).
 sub(X, Y, [], []).
+
+%%% 2(a)
+% test data:
+%   t(t(t(nil,1,nil), 2, t(nil,3,nil)), 4, t(t(nil,5,nil), 6, t(nil,7,nil)))
+less(X, t(L, K, R)) :-
+	less(X, K),
+	less(X, L),
+	less(X, R), !.
+less(t(L, K, R), X) :-
+	less(K, X),
+	less(L, X),
+	less(R, X), !.
+less(nil, X) :- !.
+less(X, nil) :- !.
+less(X, Y) :- X < Y, !.
+orderedT(t(L, K, R)) :-
+	less(L, K),
+	less(K, R).
