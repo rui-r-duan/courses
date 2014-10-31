65fafb42f9626101af02a0fe0744b9514409ae30
Ryan Duan <duanpanda@gmail.com>
2014-10-30 22:08:30 -0400

    background, rect, text, click event handling, closure

--- ai/assignment2/hanoigame.py
+++ ai/assignment2/hanoigame.py
@@ -20,50 +20,61 @@ class Disk(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.size_level = 4
-        
+
 def main():
     pygame.init()
-    screen = pygame.display.set_mode((600, 400))
+    screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption('Tower of Hanoi')
     pygame.mouse.set_visible(True)
 
+    bg_color = color1
+
+    def redraw():
+        background.fill(bg_color)
+        if pygame.font:
+            background.blit(text, textpos)
+        background.fill(font_color_red, pygame.Rect(20, 20, 100, 50))
+
+    def onMouseButtonDown(event):
+        redraw()
+
+    def onMouseButtonUp(event):
+        redraw()
+
     background = pygame.Surface(screen.get_size())
     background = background.convert()
-    background.fill(color1)
 
     if pygame.font:
         font = pygame.font.Font(None, 36)
         text = font.render("*  TOWERS OF HANOI  *", 1, (240, 240, 240))
         textpos = text.get_rect(center = (background.get_width()/2,
-                                          background.get_height()/2))
-        background.blit(text, textpos)
+                                          100))
 
+    redraw()
     screen.blit(background, (0, 0))
     pygame.display.flip()
 
     clock = pygame.time.Clock()
 
-    going = True
-    while going:
+    while 1:
         clock.tick(60)
 
         # Handle Input Events
         for event in pygame.event.get():
             if event.type == QUIT:
-                going = False
+                return
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
-                going = False
+                return
             elif event.type == MOUSEBUTTONDOWN:
-                background.fill(color2)
-                background.blit(text, textpos)
+                bg_color = color2
+                redraw()
             elif event.type == MOUSEBUTTONUP:
-                background.fill(color1)
-                background.blit(text, textpos)
+                bg_color = color1
+                redraw()
 
+        # display everything
         screen.blit(background, (0, 0))
         pygame.display.flip()
-
-    pygame.quit()
 
 if __name__ == '__main__':
     main()
