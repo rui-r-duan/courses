46c62f1ce0a364e84789f4f0cb61e5aac487e561
Ryan Duan <duanpanda@gmail.com>
2014-11-01 23:40:37 -0400

    game logic complete

--- ai/assignment2/hanoigame.py
+++ ai/assignment2/hanoigame.py
@@ -1,13 +1,12 @@
 import os
 import pygame
 import random
-import copy
+
 from pygame.locals import *
 from pygame.compat import geterror
 
-if not pygame.font: print ('Warning, fonts disabled')
-if not pygame.mixer: print ('Warning, sound disabled')
-
+#------------------------------------------------------------------------------
+# GLOBAL DATA
 main_dir = os.path.split(os.path.abspath(__file__))[0]
 
 color1 = (100, 50, 200)
@@ -16,6 +15,7 @@ color_gamearea = (255, 255, 255)
 color_peg = (10, 10, 10)
 color_active_peg = (255, 201, 14)
 color_floor = (63, 72, 204)
+color_red = (255, 0, 0)
 font_color_white = (230, 230, 230)
 font_color_grey = (68, 68, 68)
 font_color_blue = (80, 80, 180)
@@ -23,12 +23,21 @@ font_color_red = (190, 90, 100)
 
 M = 20
 L = 155
-D = 15                          # space between largest disks in adjacent pegs
+D = 15                          # space between largest discs in adjacent pegs
 PEG_W = 10
+DISC_H = 20
 
-def rand_color():
-    return [random.randrange(0,230) for i in range(0,3)]
+text = [0, 0, 0, 0, 0, 0, 0, 0]
+Topic = ("1", "2", "3", "FROM TOWER #",
+         "TO TOWER #", "MOVE #", "PRESS F1 TO EXIT")
+Coor = ((145, 70), (315, 70),
+           (485, 70), (110, 350), (320, 350), (260, 380), (220, 400))
+text1 = [0, 0, 0]
+L_MOVE = ((260, 350), (440, 350), (340, 380))
 
+#------------------------------------------------------------------------------
+# UTILITY FUNCTIONS
+#------------------------------------------------------------------------------
 def read_config_from_file(filepath):
     result = []
     cnt = 0
@@ -44,64 +53,85 @@ def read_config_from_file(filepath):
             cnt = cnt + 1
         return result
 
-DISK_H = 20
-class Disk(pygame.sprite.Sprite):
-    def __init__(self, sizelevel, gamearea):
+def rand_color():
+    return [random.randrange(0,230) for i in range(0,3)]
+
+#------------------------------------------------------------------------------
+# CLASSES
+#------------------------------------------------------------------------------
+
+class Disc(pygame.sprite.Sprite):
+    def __init__(self, sizelevel):
         pygame.sprite.Sprite.__init__(self)
         self.size_level = sizelevel
-        self.image = pygame.Surface([18*sizelevel+40, DISK_H])
-        self.image.fill(rand_color())
+        self.image = pygame.Surface([18*sizelevel+40, DISC_H])
+        self.color = rand_color()
+        self.image.fill(self.color)
         self.rect = self.image.get_rect()
-        self.rect.x = random.randrange(gamearea.x + 10,
-                                       gamearea.width - self.rect.width)
-        self.rect.y = random.randrange(gamearea.y + 10,
-                                       gamearea.height - self.rect.height)
         self.picked = False
+        self.valid = True
+
+    def __str__(self):
+        return str(self.size_level)
+
+    def set_valid(self, isvalid):
+        self.valid = isvalid
 
     def update(self):
-        "move the disk based on the mouse position"
+        "move the disc based on the mouse position"
         if self.picked:
             pos = pygame.mouse.get_pos()
             self.rect.center = pos
+            if self.valid:
+                self.image.fill(self.color)
+            else:
+                self.image.fill(self.color)
+                pygame.draw.line(self.image, color_red,
+                                 (self.rect.x, self.rect.y),
+                                 (self.rect.x + self.rect.width - 1,
+                                  self.rect.y + self.rect.height - 1),
+                                 4)
+                pygame.draw.line(self.image, color_red,
+                                 (self.rect.x + self.rect.width - 1,
+                                  self.rect.y),
+                                 (self.rect.x,
+                                  self.rect.y + self.rect.height - 1),
+                                 4)
 
 class Peg(pygame.sprite.Sprite):
-    def __init__(self, gamedata):
+    def __init__(self, game, index):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([PEG_W, (10*DISK_H)+30])
+        self.image = pygame.Surface([PEG_W, (10*DISC_H)+30])
         self.image.fill(color_peg)
         self.rect = self.image.get_rect()
-        self.disks = []
-        self.in_place_disks = []
         self.is_focused = False
-        self._gamedata = gamedata
+        self._game = game
+        self.discs = game.disc_stack_list[index]
+        self.in_place_discs = []
 
-    def re_put_disks(self):
+    def re_put_discs(self):
         self.in_place_disks = []
-        self.place_disks()
+        self.place_discs()
 
-    def put_disk(self, d):
-        length = len(self.in_place_disks)
+    def put_disc(self, d):
+        length = len(self.in_place_discs)
         d.rect.centerx = self.rect.centerx
         if length == 0:
             d.rect.y = self.rect.y + self.rect.height - d.rect.height
         else:
-            d.rect.y = self.in_place_disks[-1].rect.y - d.rect.height
-        self.in_place_disks.append(d)
+            d.rect.y = self.in_place_discs[-1].rect.y - d.rect.height
+        self.in_place_discs.append(d)
 
-    def place_disks(self):
-        for d in self.disks:    # from the largest to the smallest (in order)
-            self.put_disk(d)
-
-    def add_disk(self, d):
-        self.disks.append(d)
-        self.put_disk(d)
+    def place_discs(self):
+        for d in self.discs:    # from the largest to the smallest (in order)
+            self.put_disc(d)
 
     def become_focused(self, focused):
         self.is_focused = focused
         if focused:
-            if not self._gamedata.is_moving:
-                self.in_place_disks[-1].picked = True
-                self._gamedata.is_moving = True
+            if not self._game.is_moving:
+                self.in_place_discs[-1].picked = True
+                self._game.is_moving = True
 
     def update(self):
         if (self.is_focused):
@@ -109,64 +139,106 @@ class Peg(pygame.sprite.Sprite):
         else:
             self.image.fill(color_peg)
 
-class GameData:
+class HanoiGame:
     def __init__(self, gameconfig):
-        self.normal_stack_list = gameconfig
-        self.tmp_stack_list = copy.deepcopy(self.normal_stack_list)
-        self.from_n = 0
-        self.to_n = 0
-        self.from_peg = None       # reference to the origin Peg object
-        self.to_peg = None         # reference to the target Peg object
-        self.is_valid = True
         self.is_moving = False
-        self.step_count = 0
-        self.current_picked_disk = None # reference to the picked Disk object
-        self.active_peg = None          # reference to the active Peg object
+        self.disc_stack_list = []
+        for int_stack in gameconfig:
+            self.disc_stack_list.append(map(Disc, int_stack))
+
+    def get_disc_list(self):
+        r = []
+        for stack in self.disc_stack_list:
+            for disc in stack:
+                r.append(disc)
+        return r
 
     def is_win(self):
-        return self.normal_stack_list[0] == []\
-            and self.normal_stack_list[1] == []\
-            and len(self.normal_stack_list[2]) != 0
+        x = self.disc_stack_list[0] == []\
+            and self.disc_stack_list[1] == []\
+            and len(self.disc_stack_list[2]) != 0
+        return x
 
-        # frompeg, topeg is one of 0, 1, 2
-    def move(self, frompeg, topeg):
-        self.tmp_stack_list = copy.deepcopy(self.normal_stack_list)
-        if self.is_moving:
-            # pop from frompeg
-            x = self.tmp_stack_list[frompeg].pop()
-            # push onto topeg
-            peg = self.tmp_stack_list[topeg]
-            peg[len(peg):] = [x]
-            print self.tmp_stack_list, "valid?", self.check_valid(self.tmp_stack_list)
+    def begin_move(self):
+        print '\nbegin_move'
+        self.is_moving = True
 
-    def move_done(self):
+    def end_move(self, from_n, to_n):
+        print 'end_move: (', from_n, '->', to_n, ')'
         if self.is_moving:
+            self._move(from_n, to_n)
             self.is_moving = False
-            self.normal_stack_list = copy.deepcopy(self.tmp_stack_list)
-        else:
-            self.normal_stack_list = copy.deepcopy(self.tmp_stack_list)
-        self.step_count = self.step_count + 1
-        print "move_done: ", self.normal_stack_list
 
-    def check_valid(self, stack_list):
-        for stack in stack_list:
-            r = all(stack[i] < stack[i+1] for i in xrange(len(stack)-1))
+    # from_n, to_n is one of 0, 1, 2
+    # raise an IndexError: pop from empty list
+    def _move(self, from_index, to_index):
+        if from_index == to_index:
+            return
+        if self.is_moving:
+            # pop from self.from_n, may raise IndexError
+            disc = self.disc_stack_list[from_index].pop()
+            # push onto self.to_n
+            to_stack = self.disc_stack_list[to_index]
+            to_stack.append(disc)
+
+    def check_stacks_valid(self):
+        x = True
+        for stack in self.disc_stack_list:
+            r = all(stack[i].size_level > stack[i+1].size_level\
+                    for i in xrange(len(stack)-1))
             if r == False:
-                return r
-        return True
+                x = r
+                break
+        print 'check_stacks_valid:', x
+        return x
 
-    def check_move_valid(self, disk_to_be_put, disk_down):
-        return disk_to_be_put < disk_down
+    def is_move_valid(self, from_n, to_n):
+        stack_of_from = self.disc_stack_list[from_n]
+        stack_of_to = self.disc_stack_list[to_n]
+        if (len(stack_of_from) == 0):
+            result = False
+        elif (len(stack_of_to) == 0):
+            result = True
+        else:
+            top_of_from = stack_of_from[-1].size_level
+            top_of_to = stack_of_to[-1].size_level
+            result = (top_of_from < top_of_to)
+        print 'is_move_valid(', from_n, '->', to_n, '): ', result
+        return result
+
+    def ps(self):
+        print '[',
+        for stack in self.disc_stack_list:
+            print '[',
+            for a in stack:
+                print a,
+            print ']',
+        print ']'
+
+    def pd(self):
+        print '==============='
+        print ' disc_stack_list:', self.ps()
+        print ' is_moving:', self.is_moving
+        print '==============='
 
 def main():
+    try:
+        game_config = read_config_from_file(main_dir + '/input')
+    except IOError:
+        game_config = [[3, 2, 1], [], []]
+
     pygame.init()
     screen = pygame.display.set_mode((640, 480))
     pygame.display.set_caption('Tower of Hanoi')
     pygame.mouse.set_visible(True)
     bg_color = color1
     game_area = Rect(50, 60, 540, 360)
-    floor_area = Rect(game_area.x + M - 10, game_area.y + 30 + 10*DISK_H+30,
+    floor_area = Rect(game_area.x + M - 10, game_area.y + 30 + 10*DISC_H+30,
                       3*L+D+39, 10)
+
+    from_n = 0
+    to_n = 0
+    count = 0
 
     def draw_text():
         if pygame.font:
@@ -178,12 +250,6 @@ def main():
         background.fill(color_floor, floor_area)
         draw_text()
 
-    def onMouseButtonDown(event):
-        redraw()
-
-    def onMouseButtonUp(event):
-        redraw()
-
     background = pygame.Surface(screen.get_size())
     background = background.convert()
 
@@ -191,6 +257,8 @@ def main():
         font = pygame.font.Font(None, 26)
         title = font.render("*  TOWERS OF HANOI  *", 1, font_color_white)
         titlepos = title.get_rect(center = (background.get_width()/2, 30))
+        from_tower = font.render("FROM TOWER #", 1, font_color_grey)
+        to_tower = font.render("TO TOWER #", 1, font_color_grey)
 
     redraw()
     screen.blit(background, (0, 0))
@@ -198,45 +266,24 @@ def main():
 
     clock = pygame.time.Clock()
 
-    # prepare core game data
-    try:
-        game_config = read_config_from_file(main_dir + '/input')
-    except IOError as inst:
-        game_config = [[3, 2, 1], [], []]
-    gamedata = GameData(game_config)
+    game = HanoiGame(game_config)
 
-    # prepare game objects
+    # create pegs
     pegs = []
-    pegsgroup = None
-    disks = []
-    disksgroup = None
-    def rebuild_objs(gamedata):
+    def rebuild_objs():
         dx = L + D
         i = 0
         while i < 3:
-            pegs.append(Peg(gamedata))
+            pegs.append(Peg(game, i))
             pegs[i].rect.x = game_area.x + M + L / 2-PEG_W / 2 + i * dx
             pegs[i].rect.y = game_area.y + 30
             i = i + 1
-
-        i = 0
-        while i < len(gamedata.normal_stack_list):
-            stack = gamedata.normal_stack_list[i]
-            j = 0
-            while j < len(stack):
-                size = stack[j]
-                d = Disk(size, game_area)
-                disks.append(d)
-                pegs[i].disks.append(d)
-                j = j + 1
-            i = i + 1
-        # place disks in each peg
         for p in pegs:
-            p.re_put_disks()
+             p.re_put_discs()
 
-    rebuild_objs(gamedata)
+    rebuild_objs()
     pegsgroup = pygame.sprite.RenderPlain(tuple(pegs))
-    disksgroup = pygame.sprite.RenderPlain(tuple(disks))
+    discsgroup = pygame.sprite.RenderPlain(tuple(game.get_disc_list()))
 
     def which_peg_is_mouse_in():
         d = game_area.width/3
@@ -254,64 +301,88 @@ def main():
         else:
             return None
 
-    while 1:
+    def get_moving_disc(from_n):
+        if game.is_moving:
+            return game.disc_stack_list[from_n][-1]
+        else:
+            return None
+
+    while not game.is_win():
         clock.tick(60)
 
-        # Handle Input Events
+        # handle input events
         for event in pygame.event.get():
             if event.type == QUIT:
                 return
             elif event.type == KEYDOWN and event.key == K_ESCAPE:
                 return
+            elif event.type == KEYDOWN and event.key == 282:
+                return
             elif event.type == MOUSEBUTTONDOWN:
                 active_peg_index = which_peg_is_mouse_in()
                 if active_peg_index != None:
-                    gamedata.active_peg = pegs[active_peg_index]
-                    gamedata.active_peg.become_focused(True)
-                gamedata.is_moving = True
-                gamedata.from_peg = gamedata.active_peg
-                gamedata.from_n = active_peg_index
+                    active_peg = pegs[active_peg_index]
+                    active_peg.become_focused(True)
+                game.begin_move()
+                from_peg = active_peg
+                from_n = active_peg_index
                 redraw()
             elif event.type == MOUSEBUTTONUP:
-                if gamedata.active_peg != None:
-                    gamedata.active_peg.become_focused(False)
-                    # temporaryly put the picked disk back to original peg
-                    gamedata.from_peg.in_place_disks[-1].picked = False
-                    gamedata.from_peg.re_put_disks()
-                    gamedata.active_peg = None
-                gamedata.from_peg = None
-                gamedata.to_peg = None
+                to_n = which_peg_is_mouse_in()
+                if active_peg != None:
+                    active_peg.become_focused(False)
+                    # temporarily put the picked disc back to original peg
+                    game.disc_stack_list[from_n][-1].picked = False
+                    from_peg.re_put_discs()
+                    active_peg = None
+                from_peg = None
+                to_peg = None
+                if not game.is_move_valid(from_n, to_n):
+                    to_n = from_n
+                else:
+                    count = count + 1
+                game.end_move(from_n, to_n)
                 pegs = []
                 pegsgroup = None
-                disks = []
-                disksgroup = None
-                gamedata.move_done()
-                rebuild_objs(gamedata)
+                rebuild_objs()
                 pegsgroup = pygame.sprite.RenderPlain(tuple(pegs))
-                disksgroup = pygame.sprite.RenderPlain(tuple(disks))
                 redraw()
+                
 
-        if gamedata.is_moving:
+        if game.is_moving:
             tmp_peg_index = which_peg_is_mouse_in()
             if tmp_peg_index != active_peg_index:
-                gamedata.active_peg.become_focused(False)
+                active_peg.become_focused(False)
                 tmp_peg = pegs[tmp_peg_index]
                 tmp_peg.become_focused(True)
                 active_peg_index = tmp_peg_index
-                gamedata.active_peg = tmp_peg
-                gamedata.to_peg = gamedata.active_peg
-                gamedata.to_n = tmp_peg_index
-                print (gamedata.from_n, gamedata.to_n)
-                gamedata.move(gamedata.from_n, gamedata.to_n)
+                active_peg = tmp_peg
+                to_peg = active_peg
+                to_n = tmp_peg_index
+                is_valid_move = game.is_move_valid(from_n, to_n)
+                moving_disc = get_moving_disc(from_n)
+                if not is_valid_move:
+                    moving_disc.set_valid(False)
+                else:
+                    moving_disc.set_valid(True)
 
-        disksgroup.update()
+        discsgroup.update()
         pegsgroup.update()
 
         # display everything
         screen.blit(background, (0, 0))
         pegsgroup.draw(screen)
-        disksgroup.draw(screen)
+        discsgroup.draw(screen)
+        for i in range(len(Topic)):
+            text[i] = font.render(Topic[i], True, (0, 0, 0))
+            screen.blit(text[i], Coor[i])
+        move_record = [from_n + 1, to_n + 1, count]
+        for i in range(len(move_record)):
+            text1[i] = font.render(str(move_record[i]), True, (0, 0, 0))
+            screen.blit(text1[i], L_MOVE[i])
         pygame.display.flip()
+
+    print "You Win!"
 
 if __name__ == '__main__':
     main()
