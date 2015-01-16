705384374cde57e9a450f508aee4f8712589978a
Ryan Duan <duanpanda@gmail.com>
2015-01-16 13:07:34 -0500

    populate todo entries, and save OK.

--- mobile/challenge_1/javascripts/main.js
+++ mobile/challenge_1/javascripts/main.js
@@ -8,7 +8,11 @@ $(document).ready( function() {
 	for (var i = 0; i < users.length; i++) {
 	    if (name === users[i].name && passwd === users[i].passwd) {
 		console.log(name + " login successfully!");
+
 		bLogIn = true;
+		current_user = i;
+		console.log("user_index: " + i);
+
 		$(this).attr("href", "#act-list-page");
 		$(this).attr("data-transition", "slide");
 		break;
@@ -22,6 +26,19 @@ $(document).ready( function() {
 	}
     });
 
+    $("#btn-save").click(function() {
+	$(this).attr('href="#act-list-page"');
+	activities.push({activity: $("#activity").text(),
+			 start_time: $("#start-time").val(),
+			 end_time: $("#end-time").val(),
+			 notes: $("#note").text()});
+	var txt = '<li><a href="#">'
+		+ activities[activities.length - 1].activity + '</a></li>';
+	$("ol").append(txt);
+	$("ol").listview("refresh");
+	console.log(activities[activities.length-1]);
+    });
+
     $("#activity").text( "Homework" );
 
     $("#btn-logout").click(function() {
@@ -29,6 +46,8 @@ $(document).ready( function() {
 	$("#name").val("");
 	$("#password").val("");
     });
+
+    appendActivity();
 });
 
 var users = [
@@ -37,5 +56,25 @@ var users = [
 ];
 
 var bLogIn = false;
+var current_user = -1;
 
-var activities = [];
+var activities = [
+    {
+	activity: "Drawing",
+	start_time: "7:35 am",
+	end_time: "8:45 am",
+	notes: "read the book first"},
+    {
+	activity: "Homework",
+	start_time: "9:00 am",
+	end_time: "10:00 am",
+	notes: "Do whatever you want."}
+];
+
+function appendActivity() {
+    var txt;
+    for (var i = 0; i < activities.length; i++) {
+	txt = "<li><a href=#>" + activities[i].activity + "</a></li>";
+	$("ol").append(txt);
+    }
+}
