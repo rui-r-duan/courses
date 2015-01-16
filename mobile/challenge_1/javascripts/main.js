4126f8252a0cedce08b11492551b4ddbd31ca453
Ryan Duan <duanpanda@gmail.com>
2015-01-16 09:55:17 -0500

    login done

--- mobile/challenge_1/javascripts/main.js
+++ mobile/challenge_1/javascripts/main.js
@@ -1,6 +1,28 @@
 $(document).ready( function() {
     $("#btn-login").click( function() {
-	alert("Login succeed!");
+	var name = $("#name").val();
+	var passwd = $("#password").val();
+	var bSuc = false;
+	console.log(name);
+	console.log(passwd);
+	for (var i = 0; i < users.length; i++) {
+	    if (name === users[i].name && passwd === users[i].passwd) {
+		alert(name + " login successfully!");
+		bSuc = true;
+		break;
+	    }
+	}
+	if (!bSuc) {
+	    $("#btn-login").attr("href", "#login-fail-dialog");
+	    $("#name").val("");
+	    $("#password").val("");
+	}
     });
-    $("#category").text( "Homework" );
+
+    $("#activity").text( "Homework" );   
 });
+
+var users = [
+    {name: "ryan", passwd: "123"},
+    {name: "zhenzhen", passwd: "456"}
+];
