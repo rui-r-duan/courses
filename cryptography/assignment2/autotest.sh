87907cba456d3483b2a4bf70f9ffe097f9241764
Ryan Duan <duanpanda@gmail.com>
2015-02-09 17:26:36 -0500

    add make TAGS in makefile

--- cryptography/assignment2/autotest.sh
+++ cryptography/assignment2/autotest.sh
@@ -7,7 +7,7 @@ java -ea:ryanduan.crypt.MDES Test_P1_3 < input5
 java -ea:ryanduan.crypt.MDES Test_P1_3 < input_void
 java -ea:ryanduan.crypt.MDES Test_P1_3 < input_invalid
 
-make CBCtest
+make testCBC
 java -ea:ryanduan.crypt.MDES -ea:ryanduan.crypt.CBC Test_P2_CBC < input1
 java -ea:ryanduan.crypt.MDES -ea:ryanduan.crypt.CBC Test_P2_CBC < input2
 java -ea:ryanduan.crypt.MDES -ea:ryanduan.crypt.CBC Test_P2_CBC < input3
@@ -16,7 +16,7 @@ java -ea:ryanduan.crypt.MDES -ea:ryanduan.crypt.CBC Test_P2_CBC < input5
 java -ea:ryanduan.crypt.MDES -ea:ryanduan.crypt.CBC Test_P2_CBC < input_void
 java -ea:ryanduan.crypt.MDES -ea:ryanduan.crypt.CBC Test_P2_CBC < input_invalid
 
-make CTRtest
+make testCTR
 java -ea:ryanduan.crypt.MDES -ea:ryanduan.crypt.CTR Test_P2_CTR < input1
 java -ea:ryanduan.crypt.MDES -ea:ryanduan.crypt.CTR Test_P2_CTR < input2
 java -ea:ryanduan.crypt.MDES -ea:ryanduan.crypt.CTR Test_P2_CTR < input3
