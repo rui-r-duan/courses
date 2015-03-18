453c29a64e763a8c8b99ef00db401fdf80362975
Ryan Duan <duanpanda@gmail.com>
2015-03-18 18:10:08 -0400

    SH1withDSA signature OK, separate GenKey

--- cryptography/assignment3/Digest_Sig_Enc.java
+++ cryptography/assignment3/Digest_Sig_Enc.java
@@ -1,5 +1,5 @@
 import java.security.*;
-import java.io.IOException;
+import java.io.*;
 import ryanduan.crypto.RDUtils;
 
 public class Digest_Sig_Enc {
@@ -15,14 +15,44 @@ public class Digest_Sig_Enc {
         String in = new String(inputBuffer, 0, len); 
         System.out.print(in);
 
+        // compute MD5 digest
+        System.out.println();
+        System.out.print("MD5 Digest: ");
         try {
             MessageDigest md = MessageDigest.getInstance("MD5");
-
             md.update(inputBuffer);
             byte[] dg = md.digest();
-            System.out.print(RDUtils.byte2hex(dg));
+            System.out.println(RDUtils.byte2hex(dg));
         } catch (NoSuchAlgorithmException e) {
             System.err.println("I'm sorry, but MD5 is not supported");
+        }
+
+
+        try {
+            // Read private key for signature
+            ObjectInputStream ins = new ObjectInputStream(new FileInputStream("myprivkey.dat"));
+            PrivateKey myprivkey = (PrivateKey)ins.readObject();
+            ins.close();
+
+            // sign
+            Signature sig = Signature.getInstance("SHA1withDSA");
+            sig.initSign(myprivkey);
+            sig.update(in.getBytes());
+            byte[] signed = sig.sign();
+
+            // save the message and the signature in file "myinfo.dat"
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("myinfo.dat"));
+            out.writeObject(in);
+            out.writeObject(signed);
+            out.close();
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("No such algorithm: SHA1withDSA");
+        } catch (FileNotFoundException e) {
+            System.err.println("file not found: myinfo.dat");
+        } catch (IOException e) {
+            System.err.println(e.getMessage());
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
---
+++ cryptography/assignment3/GenKey.java
@@ -0,0 +1,35 @@
+import java.security.*;
+import java.io.*;
+
+public class GenKey {
+    public static void main(String[] args) {
+        // create and init key pair generator
+        try {
+            KeyPairGenerator keygen = KeyPairGenerator.getInstance("DSA");
+            SecureRandom secrand = SecureRandom.getInstance("SHA1PRNG", "SUN");
+            secrand.setSeed("aetf".getBytes());
+            keygen.initialize(1024, secrand); // key size: 1024
+
+            // generate public key and private key
+            KeyPair keys = keygen.generateKeyPair();
+            PublicKey pubkey = keys.getPublic();
+            PrivateKey privkey = keys.getPrivate();
+
+            // save keys in files
+            ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("myprivkey.dat"));
+            out.writeObject(privkey);
+            out.close();
+            out = new ObjectOutputStream(new FileOutputStream("mypubkey.dat"));
+            out.writeObject(pubkey);
+            out.close();
+        } catch (NoSuchAlgorithmException e) {
+            System.err.println("KeyPairGenerator is not supported");
+        } catch (NoSuchProviderException e) {
+            System.err.println("SecureRandom of SHA1PRNG from SUN is not supported");
+        } catch (FileNotFoundException e) {
+            System.err.println(e.getMessage());
+        } catch (IOException e) {
+            System.err.println("IOException");
+        }
+    }
+}
