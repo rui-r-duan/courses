ddf1e25d373197f5f351955d701a97ad0ed3fc55
Ryan Duan <duanpanda@gmail.com>
2015-03-27 13:58:28 -0400

    Use ExpandListView in MyMealsActivity

--- mobile/project/AndroNut/app/src/main/java/ca/lakeheadu/ryanduan/andronut/MyMealsActivity.java
+++ mobile/project/AndroNut/app/src/main/java/ca/lakeheadu/ryanduan/andronut/MyMealsActivity.java
@@ -8,35 +8,91 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ExpandableListView;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class MyMealsActivity extends ActionBarActivity {
     public final static String SELECTED_MEAL = "ca.lakeheadu.ryanduan.andronut.SELECTED_MEAL";
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_meals);
         ListView list = (ListView)findViewById(R.id.mymeals_listview);
-        String[] menuItems = new String[] {
-                "Breakfast", "Lunch", "Supper"
-        };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_list_item_1, menuItems);
-        list.setAdapter(adapter);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                Intent intent = new Intent(MyMealsActivity.this, MealDetailActivity.class);
-                String msg = (String) ((TextView)view).getText();
-                intent.putExtra(SELECTED_MEAL, msg);
-                startActivity(intent);
-            }
-        });
+
+        expListView = (ExpandableListView) findViewById(R.id.mymeals_listview);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(this, listDataHeader, listDataChild);
+        expListView.setAdapter(listAdapter);
+
+//        String[] menuItems = new String[] {
+//                "Breakfast", "Lunch", "Supper"
+//        };
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+//                android.R.layout.simple_list_item_1, menuItems);
+//        list.setAdapter(adapter);
+//        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//            @Override
+//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+//                Intent intent = new Intent(MyMealsActivity.this, MealDetailActivity.class);
+//                String msg = (String) ((TextView)view).getText();
+//                intent.putExtra(SELECTED_MEAL, msg);
+//                startActivity(intent);
+//            }
+//        });
     }
 
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Breakfast");
+        listDataHeader.add("Lunch");
+        listDataHeader.add("Supper");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250);
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
